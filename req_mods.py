ff8743286b92f41a2457973cfcf2439f536be7ba
Tim Manochehri <tcadmin@comcast.net>
2021-11-30 17:06:18 -0800

    updated README.md and added profiles_settings.xml.

--- req_mods.py
+++ req_mods.py
@@ -12,6 +12,9 @@ def ensure_enviroment_excepthook(exceptionType, exception, traceback):
         print("Please install the following Python Modules")
         # print('This script can only be run if all modules are installed : {0}'.format(exception.msg), file=sys.stderr)
         print(*required, sep=", ")
+        print("\n\n Note: Some modules may already be part of Python3 \n and cannot be installed with pip.\n Please take note.")
+        print("For Python 3.8 in Ubuntu 20.04 recent update, only the following needed to installed.")
+        print("pip install netaddr resource argparse netifaces nmap portscan")
         exit(1)
     return old_excepthook(exceptionType, exception, traceback)
 
