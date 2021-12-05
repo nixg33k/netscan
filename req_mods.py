79902ce54b9cae1c8e1e25c8265e794fa3376269
Tim Manochehri <tcadmin@comcast.net>
2021-12-05 09:46:45 -0800

    doing Python Modules checks first.

--- req_mods.py
+++ req_mods.py
@@ -14,7 +14,7 @@ def ensure_enviroment_excepthook(exceptionType, exception, traceback):
         # print('This script can only be run if all modules are installed : {0}'.format(exception.msg), file=sys.stderr)
         print(*required, sep=", ")
         print("\n\n Note: Some modules may already be part of Python3 \n and cannot be installed with pip.\n Please take note.")
-        print("For Python 3.8 in Ubuntu 20.04 recent update, only the following needed to installed.")
+        print("For Python 3.7 in Ubuntu 20.04 recent update, only the following needed to installed.")
         print("pip install netaddr resource argparse netifaces python-nmap portscan")
         exit(1)
     return old_excepthook(exceptionType, exception, traceback)
