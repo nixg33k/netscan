af78861b5fc8528abd433dd51af695f06aae951f
Tim Manochehri <tcadmin@comcast.net>
2021-12-05 10:20:00 -0800

    doing Python Modules checks first and listing the required modules.

--- req_mods.py
+++ req_mods.py
@@ -5,7 +5,7 @@ old_excepthook = sys.excepthook
 
 def ensure_enviroment_excepthook(exceptionType, exception, traceback):
     required = {'socket', 'time', 'os', 'netifaces', 'netaddr', 'python-nmap', 'pprint', 're', 'subprocess', 'logging',
-                'argparse', 'resource', 'pkg_resources', 'netaddr', 'portscan', 'fluffy'}
+                'argparse', 'resource', 'pkg_resources', 'netaddr', 'portscan', 'distro'}
 
     # if exceptionType == ModuleNotFoundError or exceptionType == ImportError:
     if exceptionType == ImportError:
@@ -14,8 +14,9 @@ def ensure_enviroment_excepthook(exceptionType, exception, traceback):
         # print('This script can only be run if all modules are installed : {0}'.format(exception.msg), file=sys.stderr)
         print(*required, sep=", ")
         print("\n\n Note: Some modules may already be part of Python3 \n and cannot be installed with pip.\n Please take note.")
-        print("For Python 3.7 in Ubuntu 20.04 recent update, only the following needed to installed.")
-        print("pip install netaddr resource argparse netifaces python-nmap portscan")
+        print("For Python 3.5+ in Ubuntu 20.04 recent update, only the following needed to installed.")
+        print("pip install netaddr netifaces python-nmap portscan distro")
+        print("\nAlso you must install the nmap OS package.")
         exit(1)
     return old_excepthook(exceptionType, exception, traceback)
 
