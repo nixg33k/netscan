fed9fccfd2b10b974464202f6c0a1843a734440e
Tim Manochehri <tcadmin@comcast.net>
2021-12-06 10:00:09 -0800

    change to -sn from -sP as requested by nmap application.

--- netscan.py
+++ netscan.py
@@ -216,7 +216,8 @@ def get_address_in_network():
 
             # a = nm.scan(hosts=str(cidr), arguments='-T4 -sS -PE --min-rate 1000 --max-retries 1')
             # -sS required root
-            a = nm.scan(hosts=str(cidr), arguments='-T4 -sP -PE -vv --min-rate 1000 --max-retries 1')
+            # a = nm.scan(hosts=str(cidr), arguments='-T4 -sP -PE -vv --min-rate 1000 --max-retries 1')
+            a = nm.scan(hosts=str(cidr), arguments='-T4 -sn -PE -vv --min-rate 1000 --max-retries 1')
 
             endtime = time.time()
             totaltime = endtime - starttime
