94ac94ba4477112b5255bda219544d86981c2393
Tim Manochehri <tcadmin@comcast.net>
2021-12-07 11:09:32 -0800

    update number of open files.

--- softhard.py
+++ softhard.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#resource.setrlimit(resource.RLIMIT_NOFILE, (30000, 30000))
+# resource.setrlimit(resource.RLIMIT_NOFILE, (30000, 30000))
 
 import resource
 import distro
@@ -8,17 +8,16 @@ import distro
 getdistro = distro.id()
 getdistro = getdistro.replace("'", "")
 
+soft, hard = resource.getrlimit(resource.RLIMIT_OFILE)
+
 if getdistro == 'centos':
 	print("Host OS is CentOS")
-	soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
 	print("Current Open File settings - Soft: %s, Hard: %s" % (soft, hard))
 	if hard < 30000:
 		resource.setrlimit(resource.RLIMIT_NOFILE, (30000, 30000))
-	# if soft < 30000:
-	#	resource.setrlimit(resource.RLIMIT_NOFILE, (30000, hard))
 
 else:
-	soft, hard = resource.getrlimit(resource.RLIMIT_OFILE)
+	# soft, hard = resource.getrlimit(resource.RLIMIT_OFILE)
 	print("Current Open File settings - Soft: %s, Hard: %s" % (soft, hard))
 	if hard < 30000:
 		resource.setrlimit(resource.RLIMIT_OFILE, (soft, 30000))
@@ -31,22 +30,20 @@ print("Current Open File settings after change - Soft: %s, Hard: %s" % (soft2, h
 
 if getdistro == 'centos':
 	if soft2 == 30000:
-	    	print("reverting soft Open files to original setting %d" % soft)
-    		resource.setrlimit(resource.RLIMIT_NOFILE, (soft, hard))
+		print("reverting soft Open files to original setting %d" % soft)
+		resource.setrlimit(resource.RLIMIT_NOFILE, (soft, hard))
 	if hard2 == 30000:
-	    	print("reverting hard Open files to original setting %d" % hard)
-    		resource.setrlimit(resource.RLIMIT_NOFILE, (soft, hard))
+		print("reverting hard Open files to original setting %d" % hard)
+		resource.setrlimit(resource.RLIMIT_NOFILE, (soft, hard))
 	
 	soft3, hard3 = resource.getrlimit(resource.RLIMIT_NOFILE)
 else:
-        if soft2 == 30000:
-                print("reverting soft Open files to original setting %d" % soft)
-                resource.setrlimit(resource.RLIMIT_OFILE, (soft, hard))
-        if hard2 == 30000:
-                print("reverting hard Open files to original setting %d" % hard)
-                resource.setrlimit(resource.RLIMIT_OFILE, (soft, hard))
+	if soft2 == 30000:
+		print("reverting soft Open files to original setting %d" % soft)
+		resource.setrlimit(resource.RLIMIT_OFILE, (soft, hard))
+	if hard2 == 30000:
+		print("reverting hard Open files to original setting %d" % hard)
+		resource.setrlimit(resource.RLIMIT_OFILE, (soft, hard))
 	
-        soft3, hard3 = resource.getrlimit(resource.RLIMIT_OFILE)
-
-
+soft3, hard3 = resource.getrlimit(resource.RLIMIT_OFILE)
 print("Reverting Open File settings - Soft: %s, Hard: %s" % (soft3, hard3))
