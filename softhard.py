0287ca1f4fa15617b38c96c96da8284edfe23651
Tim Manochehri <tcadmin@comcast.net>
2021-12-07 10:57:47 -0800

    display time in microseconds when port scanning.

---
+++ softhard.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+#resource.setrlimit(resource.RLIMIT_NOFILE, (30000, 30000))
+
+import resource
+import distro
+
+getdistro = distro.id()
+getdistro = getdistro.replace("'", "")
+
+if getdistro == 'centos':
+	print("Host OS is CentOS")
+	soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
+	print("Current Open File settings - Soft: %s, Hard: %s" % (soft, hard))
+	if hard < 30000:
+		resource.setrlimit(resource.RLIMIT_NOFILE, (30000, 30000))
+	# if soft < 30000:
+	#	resource.setrlimit(resource.RLIMIT_NOFILE, (30000, hard))
+
+else:
+	soft, hard = resource.getrlimit(resource.RLIMIT_OFILE)
+	print("Current Open File settings - Soft: %s, Hard: %s" % (soft, hard))
+	if hard < 30000:
+		resource.setrlimit(resource.RLIMIT_OFILE, (soft, 30000))
+	if soft < 30000:
+		resource.setrlimit(resource.RLIMIT_OFILE, (30000, hard))
+
+
+soft2, hard2 = resource.getrlimit(resource.RLIMIT_OFILE)
+print("Current Open File settings after change - Soft: %s, Hard: %s" % (soft2, hard2))
+
+if getdistro == 'centos':
+	if soft2 == 30000:
+	    	print("reverting soft Open files to original setting %d" % soft)
+    		resource.setrlimit(resource.RLIMIT_NOFILE, (soft, hard))
+	if hard2 == 30000:
+	    	print("reverting hard Open files to original setting %d" % hard)
+    		resource.setrlimit(resource.RLIMIT_NOFILE, (soft, hard))
+	
+	soft3, hard3 = resource.getrlimit(resource.RLIMIT_NOFILE)
+else:
+        if soft2 == 30000:
+                print("reverting soft Open files to original setting %d" % soft)
+                resource.setrlimit(resource.RLIMIT_OFILE, (soft, hard))
+        if hard2 == 30000:
+                print("reverting hard Open files to original setting %d" % hard)
+                resource.setrlimit(resource.RLIMIT_OFILE, (soft, hard))
+	
+        soft3, hard3 = resource.getrlimit(resource.RLIMIT_OFILE)
+
+
+print("Reverting Open File settings - Soft: %s, Hard: %s" % (soft3, hard3))
