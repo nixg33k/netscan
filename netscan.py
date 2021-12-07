6462463dfea9110972915399ea6b7d4d20c8ff75
Tim Manochehri <tcadmin@comcast.net>
2021-12-07 10:50:04 -0800

    display time in microseconds when port scanning.

--- netscan.py
+++ netscan.py
@@ -355,7 +355,7 @@ def main():
             resource.setrlimit(resource.RLIMIT_OFILE, (soft, hard))
 
         if soft2 == 30000 or hard2 == 30000:
-            soft3, hard3 = resource.getrlimit(resource.RLIMIT_OFILE)
+            soft3, hard3 = resource.getrlimit(resource.RLIMIT_NOFILE)
             print("Reverting Open File settings - Soft: %s, Hard: %s" % (soft3, hard3))
 
 
